918a5aaf95778a5b5e45d286f459e7ad7bec6524
showzen <showzen97@gmail.com>
2018-11-20 22:48:24 +0000

    scripts

---
+++ BigHeadWarriors/Assets/Scripts/FirebaseAuthenticationScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase;
+using Firebase.Auth;
+using UnityEngine.SceneManagement;
+
+
+public class FirebaseAuthenticationScript : MonoBehaviour {
+
+    public InputField email, password;
+
+    public void LoginButtonPressed()
+    {
+        Debug.Log("hey");
+        FirebaseAuth.DefaultInstance.SignInWithEmailAndPasswordAsync(email.text, password.text).
+            ContinueWith((obj) =>
+            {
+                SSTools.ShowMessage(email.text, SSTools.Position.bottom, SSTools.Time.oneSecond);
+                SceneManager.LoadSceneAsync("MainGame");
+            });
+
+    }
+
+    public void CreateUserButtonPressed()
+    {
+        FirebaseAuth.DefaultInstance.CreateUserWithEmailAndPasswordAsync(email.text,password.text).
+             ContinueWith((obj) =>
+             { 
+                 SceneManager.LoadSceneAsync("MainGame");
+             });
+    }
+}
